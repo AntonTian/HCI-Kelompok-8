6cd49e7e05396cca9ec677968e20c119c9a0bfa2
AntonTian <109430939+AntonTian@users.noreply.github.com>
2024-06-16 12:46:34 +0700

    Delete Register directory

--- Register/water.js
+++
@@ -1,8 +0,0 @@
-"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js">
-"https://cdnjs.cloudflare.com/ajax/libs/jquery.ripples/0.5.3/jquery.ripples.min.js">
-        $(".full-landing-image").ripples({
-            resolution: 512,
-            dropRadius: 20,
-            interactive: true,
-            perturbance: 0.02,
-        });
