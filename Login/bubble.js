c669bd5dca08833694dfe282ba448e5f69566940
AntonTian <109430939+AntonTian@users.noreply.github.com>
2024-06-16 18:59:25 +0700

    Add files via upload

---
+++ Login/bubble.js
@@ -0,0 +1,16 @@
+function createBubble() { 
+    const section = document.querySelector("Section"); 
+    const createElement = document.createElement("span"); 
+    var size = Math.random() * 60; 
+
+    createElement.style.width = 180 + size + "px"; 
+    createElement.style.height = 180 + size + "px"; 
+    createElement.style.left =  
+      Math.random() * innerWidth + "px"; 
+    section.appendChild(createElement); 
+
+    setTimeout(() => { 
+        createElement.remove(); 
+    }, 4000); 
+} 
+setInterval(createBubble, 1500); 
